2095d852d02792972978d5031a6b8a0ef64212a0
lideryasin <32715938+lideryasin@users.noreply.github.com>
2018-08-27 13:32:51 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import * as firebase from 'firebase'
 
 var config = {
-    apiKey: "AIzaSyAyoQ3B2tSkTLoNftjQ6ZDkM_v7MTZwI4o",
-    authDomain: "denemeler-27522.firebaseapp.com",
-    databaseURL: "https://denemeler-27522.firebaseio.com",
-    projectId: "denemeler-27522",
-    storageBucket: "denemeler-27522.appspot.com",
-    messagingSenderId: "114773529643"
+
 };
 firebase.initializeApp(config);
 
